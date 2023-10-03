db008b46a328f351a7bdd9c2f93054a7b10eb5ac
Santiago Aguirre <58629459+santiagoaguirreg@users.noreply.github.com>
2023-10-02 22:31:14 -0500

    Iniciando Layout

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import {PrismaClient } from '@prisma/client'
+import Layout from '../layout/Layout';
 
 
-export default function Home({categorias}) {
-
-  console.log(categorias)
-  return <h1>next js</h1>;
+export default function Home() {
+  return (
+    <Layout>
+      <h1>Inicio</h1>
+    </Layout>
+  )
 }
 
-export const getServerSideProps = async () => {
-  const prisma = new PrismaClient();
 
-  const categorias = await prisma.categoria.findMany();
-
-return {
-  props: {
-    categorias,
-  },
-};
-};
 
