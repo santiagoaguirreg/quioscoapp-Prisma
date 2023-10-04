24f7a659fd9e495886908932909a52f8a9de18b1
Santiago Aguirre <58629459+santiagoaguirreg@users.noreply.github.com>
2023-10-03 23:51:35 -0500

    Api de productos

--- pages/api/categorias.js
+++ pages/api/categorias.js
@@ -4,7 +4,12 @@ export default async function handler(req, res) {
 
   const prisma = new PrismaClient()
 
-  const categorias = await prisma.categoria.findMany();
+  const categorias = await prisma.categoria.findMany({
+    include: {
+      productos: true,
+    }
+  });
 
   res.status(200).json(categorias);
 }
+
