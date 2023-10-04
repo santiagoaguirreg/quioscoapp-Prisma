44d8bcf93a6d3539d856b668295596cd8d688620
Santiago Aguirre <58629459+santiagoaguirreg@users.noreply.github.com>
2023-10-03 21:05:00 -0500

    Iniciando Sidebar

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
 
 const Layout = ({children, pagina}) => {
   return (
@@ -10,7 +11,7 @@ const Layout = ({children, pagina}) => {
 
     <div className='md:flex'>
         <aside className='md:w-4/12 xl:w-1/4 2xl:w-1/5'>
-          <h1>Sidebar Aquí</h1>
+          <Sidebar />
         </aside>
 
         <main className='md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll'>
