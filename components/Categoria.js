f2ca6654345e95c5354ebd73420bc6f4fe2b97a8
Santiago Aguirre <58629459+santiagoaguirreg@users.noreply.github.com>
2023-10-03 21:29:32 -0500

    Continuamos Sidebar

--- components/Categoria.js
+++ components/Categoria.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
+import useQuiosco from "../hooks/useQuiosco";
 
 const Categoria = ({categoria}) => {
 
- 
+    const {  categoriaActual,  handleClickCategoria } = useQuiosco()
 
     const {nombre, icono, id } = categoria;
   return (
-    <div className="flex items-center gap-4 w-full border p-5 hover:bg-amber-400">
+    <div className={`${categoriaActual?.id === id ? 'bg-amber-400' : "" } flex items-center gap-4 w-full border p-5 hover:bg-amber-400`}>
         <Image 
           width={70}
           height={70}
@@ -17,6 +18,7 @@ const Categoria = ({categoria}) => {
         <button
             type="button"
             className="text-2xl font-bold hover:cursor-pointer"
+            onClick={()=> handleClickCategoria(id)}
         >
             {nombre}
 
