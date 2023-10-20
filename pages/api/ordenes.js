181c73444b758a576e61a6f46eb226c395c4bde5
Santiago Aguirre <58629459+santiagoaguirreg@users.noreply.github.com>
2023-10-19 21:57:15 -0500

    Empezando panel admin

--- pages/api/ordenes.js
+++ pages/api/ordenes.js
@@ -1,19 +1,26 @@
-import { Prisma, PrismaClient } from "@prisma/client"
-
+import { PrismaClient } from "@prisma/client";
+ 
 export default async function handler(req, res) {
-
     const prisma = new PrismaClient();
-    if(req.method === "POST") {
-
+ 
+    if (req.method === 'GET') {
+        // Obtener Ordenes 
+        const ordenes = await prisma.orden.findMany({
+            where: {
+                estado: false
+            }
+        });
+        res.status(200).json(ordenes);
+    } else if (req.method === 'POST') {
+        // Crear Ordenes
         const orden = await prisma.orden.create({
             data: {
                 nombre: req.body.nombre,
-                total: req.body.total, 
+                total: req.body.total,
                 pedido: req.body.pedido,
-                fecha: req.body.fecha
-            }
-        })
-        res.json(orden)
+                fecha: req.body.fecha,
+            },
+        });
+        res.status(200).json(orden);
     }
-
 }
